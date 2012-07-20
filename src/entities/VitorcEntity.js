71a9ff68cb678d705521ee9a43e80a1adf532a73
newagebegins <newagebegins@gmail.com>
2012-07-20 13:30:59 +1100

    move vitorc left and right

--- src/entities/VitorcEntity.js
+++ src/entities/VitorcEntity.js
@@ -16,11 +16,48 @@ define(
       
       this.addAnimation("move", [0,1,2,3,4,0,5,6,7,8]);
       this.setCurrentAnimation("move");
+      this.animationspeed = 2;
+      
+      this.setVelocity(1.5, 0);
+      
+      this.state = "stand";
+      this.updated = false;
     },
     
     update: function () {
-      this.parent();
-      return true;
+      this.handleInput();
+      
+      if (this.state == "walkRight") {
+        this.doWalk(false);
+        this.parent();
+        this.updated = true;
+      }
+      else if (this.state == "walkLeft") {
+        this.doWalk(true);
+        this.parent();
+        this.updated = true;
+      }
+      else if (this.state == "stand") {
+        this.vel.x = 0;
+        this.setAnimationFrame(0);
+        this.updated = true;
+      }
+      
+      this.updateMovement();
+      
+      return this.updated;
+    },
+    
+    handleInput: function () {
+      if (me.input.isKeyPressed("right")) {
+        this.state = "walkRight";
+      }
+      else if (me.input.isKeyPressed("left")) {
+        this.state = "walkLeft";
+      }
+      else {
+        this.state = "stand";
+      }
     },
     
   });
