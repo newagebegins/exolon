559de8a1b5e34cc5ddd6a41223bc17709a305fea
newagebegins <newagebegins@gmail.com>
2012-08-01 18:34:08 +1100

    UpAndDownMovementBehavior: use checkpoints

--- src/behaviors/UpAndDownMovementBehavior.js
+++ src/behaviors/UpAndDownMovementBehavior.js
@@ -1,60 +1,48 @@
 define(
   [
+    "src/me",
     "src/util",
   ],
   function (
+    me,
     util
   ) {
-      
-  var VERTICAL_MOVEMENT = 80;
-  var STOP_X = 336;
-  var SPEED_X_NORMAL = 1.5;
-  var SPEED_X_FAST = 4;
-  var SPEED_Y = 1;
-  var STATES = {
-    LEFT_1: 1,
-    UP: 2,
-    DOWN: 3,
-    LEFT_2: 4,
-  };
       
   var UpAndDownMovementBehavior = Object.extend({
     
     init: function (host) {
       this.host = host;
-      this.state = STATES.LEFT_1;
+      
+      this.checkpoints = [
+        {
+          vel: new me.Vector2d(-1.5, 0),
+          distance: 176,
+        },
+        {
+          vel: new me.Vector2d(0, -1),
+          distance: 80,
+        },
+        {
+          vel: new me.Vector2d(0, 1),
+          distance: 80 - util.getRandomInt(0, 32),
+        },
+        {
+          vel: new me.Vector2d(-4, 0),
+        },
+      ];
+  
+      this.checkpoint = 0;
+      this.passedDistance = 0;
     },
     
     update: function () {
-      if (this.state == STATES.LEFT_1) {
-        this.host.pos.x -= SPEED_X_NORMAL;
-        
-        if (this.host.pos.x <= STOP_X) {
-          this.host.pos.x = STOP_X;
-          this.initY = this.host.pos.y;
-          this.ceilY = this.host.pos.y - VERTICAL_MOVEMENT;
-          this.state = STATES.UP;
-        }
-      }
-      else if (this.state == STATES.UP) {
-        this.host.pos.y -= SPEED_Y;
-        
-        if (this.host.pos.y <= this.ceilY) {
-          this.host.pos.y = this.ceilY;
-          this.floorY = this.initY - util.getRandomInt(0, 32);
-          this.state = STATES.DOWN;
-        }
-      }
-      else if (this.state == STATES.DOWN) {
-        this.host.pos.y += SPEED_Y;
-        
-        if (this.host.pos.y >= this.floorY) {
-          this.host.pos.y = this.floorY;
-          this.state = STATES.LEFT_2;
-        }
-      }
-      if (this.state == STATES.LEFT_2) {
-        this.host.pos.x -= SPEED_X_FAST;
+      var cp = this.checkpoints[this.checkpoint];
+      this.host.pos.add(cp.vel);
+      this.passedDistance += cp.vel.length();
+      
+      if (this.checkpoint < this.checkpoints.length - 1 && this.passedDistance >= cp.distance) {
+        this.passedDistance = 0;
+        this.checkpoint++;
       }
     },
     
