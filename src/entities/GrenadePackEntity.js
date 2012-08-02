3bab572770a0da31808e789c43509b4be55e3a04
newagebegins <newagebegins@gmail.com>
2012-08-03 00:09:13 +1100

    add pick1 sound

--- src/entities/GrenadePackEntity.js
+++ src/entities/GrenadePackEntity.js
@@ -26,6 +26,8 @@ define(
       this.collidable = false;
       me.game.remove(this);
       util.setGrenades(config.initialGrenades);
+      
+      me.audio.play("pick1");
     },
     
   });
--- src/resources/sound.js
+++ src/resources/sound.js
@@ -43,6 +43,12 @@ define(function () {
       src: "sound/",
       channel: 3,
     },
+    {
+      name: "pick1",
+      type: "audio",
+      src: "sound/",
+      channel: 2,
+    },
   ];
   
   return sound;
